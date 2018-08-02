f36ec8e304d894325a3b6de41753be49c5e0b584
Iris Guo <irischangguo@gmail.com>
2018-08-01 23:33:38 -0400

    Add components server+servers+success-alert+warning-alert

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+///<reference path="../../../node_modules/@angular/core/src/metadata/directives.d.ts"/>
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ my-first-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,16 @@
+///<reference path="../../../node_modules/@angular/core/src/metadata/directives.d.ts"/>
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
